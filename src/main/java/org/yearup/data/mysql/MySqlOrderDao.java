7543d26e8e08263fb9dc152b1e4cc537383c3746
Stephen_A <ankustephen99@gmail.com>
2025-12-19 07:41:34 -0500

    Added necessary CRUD methods to MySqlOrderDao Class

--- src/main/java/org/yearup/data/mysql/MySqlOrderDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlOrderDao.java
@@ -1,11 +1,15 @@
 package org.yearup.data.mysql;
 
+import org.springframework.stereotype.Component;
 import org.yearup.data.OrderDao;
 import org.yearup.models.Order;
 import org.yearup.models.OrderLineItem;
 
 import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.sql.*;
 
+@Component
 public class MySqlOrderDao extends MySqlDaoBase implements OrderDao {
     public MySqlOrderDao(DataSource dataSource) {
         super(dataSource);
@@ -13,16 +17,91 @@ public class MySqlOrderDao extends MySqlDaoBase implements OrderDao {
 
     @Override
     public Order create(Order order) {
-        return null;
+        String query = "INSERT INTO orders (user_id, date, address, city, state, zip, shipping_amount) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            statement.setInt(1, order.getUserId());
+            statement.setDate(2, order.getDate());
+            statement.setString(3, order.getAddress());
+            statement.setString(4, order.getCity());
+            statement.setString(5, order.getState());
+            statement.setString(6, order.getZip());
+            statement.setBigDecimal(7, order.getShippingAmount());
+
+            int rows = statement.executeUpdate();
+
+            if (rows == 0) {
+                throw new SQLException("Insert failed, no rows affected!");
+            }
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    int orderId = generatedKeys.getInt(1);
+                    order.setOrderId(orderId);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return order;
     }
 
     @Override
     public OrderLineItem addOrderLineItem(OrderLineItem lineItem) {
-        return null;
+        String query = "INSERT INTO order_line_items (order_id, product_id, sales_price, quantity, discount) VALUES (?, ?, ?, ?, ?)";
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            statement.setInt(1, lineItem.getOrderId());
+            statement.setInt(2, lineItem.getProductId());
+            statement.setBigDecimal(3, lineItem.getSalesPrice());
+            statement.setInt(4, lineItem.getQuantity());
+            statement.setBigDecimal(5, lineItem.getDiscount());
+
+            int rows = statement.executeUpdate();
+
+            if (rows == 0) {
+                throw new SQLException("Insert failed, no rows affected!");
+            }
+
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    int lineItemId = generatedKeys.getInt(1);
+                    lineItem.setOrderLineItemId(lineItemId);
+                }
+            }
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return lineItem;
     }
 
     @Override
     public Order getById(int orderId) {
-        return null;
+        Order order = null;
+        String query = "SELECT * FROM orders WHERE order_id = ?";
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, orderId);
+            ResultSet resultSets = statement.executeQuery();
+
+            while (resultSets.next()) {
+                int id = resultSets.getInt("order_id");
+                int userId = resultSets.getInt("user_id");
+                Date date = resultSets.getDate("date");
+                String address = resultSets.getString("address");
+                String city = resultSets.getString("city");
+                String state = resultSets.getString("state");
+                String zip = resultSets.getString("zip");
+                BigDecimal shippingAmount = resultSets.getBigDecimal("shipping_amount");
+
+                order = new Order(id, userId, date, address, city, state, zip, shippingAmount);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return order;
     }
 }
